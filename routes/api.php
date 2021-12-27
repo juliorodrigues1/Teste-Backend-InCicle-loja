547bc0f7bd625cdc77619815ebf7b17e2a50d6c8
juliorodrigues1 <juliorodrigues.ac@gmail.com>
2021-12-27 11:06:58 -0500

    CRUD de produtos

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('produto', [\App\Http\Controllers\ProductController::class, 'create']);
+Route::put('produto/{id}', [\App\Http\Controllers\ProductController::class, 'update']);
+Route::delete('produto/{id}', [\App\Http\Controllers\ProductController::class, 'destroy']);
+Route::get('produto/{id}', [\App\Http\Controllers\ProductController::class, 'show']);
+Route::get('produto', [\App\Http\Controllers\ProductController::class, 'index']);
